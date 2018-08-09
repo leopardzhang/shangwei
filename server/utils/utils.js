166b7faf5c7b3768776d7967b1ea4b09cf54178b
leopardzhang <312035716@qq.com>
2018-08-09 08:59:56 +0800

    修改了创建文件夹日期错误的问题

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -1,6 +1,6 @@
 const year = new Date().getFullYear();
 const month = new Date().getMonth() + 1 >= 10 ? new Date().getMonth() + 1 : `0${new Date().getMonth() + 1}`;
-const day = new Date().getDate() + 1 >= 10 ? new Date().getDate() + 1 : `0${new Date().getDate() + 1}`;
+const day = new Date().getDate() >= 10 ? new Date().getDate() : `0${new Date().getDate()}`;
 
 const folderName = `${year}.${month}.${day}`;
 
